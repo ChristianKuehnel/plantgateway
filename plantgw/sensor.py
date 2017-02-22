93c26aada4f316e8979b9f0a53376b32bf344f93
Christian Kühnel <christian.kuehnel@gmail.com>
2017-02-22 22:55:09 +0100

    parsing is working finally

--- plantgw/sensor.py
+++ plantgw/sensor.py
@@ -1,7 +1,6 @@
-
-
-from bluepy.btle import Peripheral
+from bluepy.btle import Peripheral,BTLEException
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -13,42 +12,65 @@ class Sensor(object):
 
     def __init__(self, mac):
         self.peripheral = Peripheral(mac)
+        logger.debug('connected to device {}'.format(mac))
         self.battery = None
         self.version = None
         self.temperature = None
         self.brightness = None
         self.moisture = None
         self.conductivity = None
-        self.fetch_38()
-        self.fetch_35()
 
-    def fetch_38(self):
-        result = self.peripheral.readCharacteristic(0x38)
+    def get_data(self):
+        self._fetch_35()
+        self._fetch_38()
+
+    def _fetch_38(self):
+        result = self._retry( self.peripheral.readCharacteristic(0x38) )
         self.battery = int.from_bytes(result[0:1],byteorder=BYTEORDER)
         self.version = result[2:7].decode('ascii')
         logger.debug('Raw data for char 0x38: {}'.format(self._format_bytes(result)))
         logger.debug('battery: {}'.format(self.battery))
         logger.debug('version: {}'.format(self.version))
 
-    def fetch_35(self):
-        result = self.peripheral.writeCharacteristic(0x33, bytes.fromhex('A01F'),withResponse=True)
-        if result['rsp'] != ['wr']:
-            logger.error('writing failed')
-            #TODO improve error handling
-        result = self.peripheral.readCharacteristic(0x35)
-        if result == INVALID_DATA:
-            logger.error('data invalid')
-            #TODO improve error handling
-        self.temperature = int.from_bytes(result[0:1],byteorder=BYTEORDER)/10.0
-        self.brightness = int.from_bytes(result[3:4],byteorder=BYTEORDER)
-        self.moisture = int.from_bytes(result[7:7],byteorder=BYTEORDER)
-        self.conductivity = int.from_bytes(result[8:9],byteorder=BYTEORDER)
+    def _fetch_35(self):
+        self._retry( self.peripheral.writeCharacteristic(0x33, bytes.fromhex('A01F'), withResponse=True) )
 
+        result = self._retry(self.peripheral.readCharacteristic(0x35) )
         logger.debug('Raw data for char 0x35: {}'.format(self._format_bytes(result)))
+
+        if result == INVALID_DATA:
+            msg = 'invalid data received'
+            logger.error(msg)
+            raise Exception(msg)
+
+        # negative numbers are stored in one's complement
+        temp_bytes = result[0:2]
+        if temp_bytes[1] & 0x80 > 0:
+            temp_bytes = [temp_bytes[0] ^ 0xFF, temp_bytes[1] ^ 0xFF]
+
+        # the temperature needs to be scaled by factor of 0.1
+        self.temperature = int.from_bytes(temp_bytes,byteorder=BYTEORDER)/10.0
+        self.brightness = int.from_bytes(result[3:5],byteorder=BYTEORDER)
+        self.moisture = int.from_bytes(result[7:8],byteorder=BYTEORDER)
+        self.conductivity = int.from_bytes(result[8:10],byteorder=BYTEORDER)
+
         logger.debug('temp: {}'.format(self.temperature))
         logger.debug('brightness: {}'.format(self.brightness))
         logger.debug('conductivity: {}'.format(self.conductivity))
         logger.debug('moisture: {}'.format(self.moisture))
+
+    @staticmethod
+    def _retry(func,num_tries=5,sleep_time=0.5):
+        for i in range(0,num_tries):
+            try:
+                return func
+            except BTLEException as e:
+                logger.exception()
+                time.sleep(sleep_time)
+                continue
+            break
+        logger.error('retry finally failed!')
+        raise
 
     @staticmethod
     def _format_bytes(b):
@@ -62,9 +84,3 @@ class Sensor(object):
             if count%8 == 0:
                 result += ' '
         return result
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    s = Sensor('C4:7C:8D:60:D6:1C')
-    #s = Sensor('C4:7C:8D:60:D6:41')
