cc48fb081e43dd79c1094a5237507ad34160798c
Peter Basham <peter_basham@yahoo.co.uk>
2017-08-11 18:29:23 +0100

    Added config options for Mqtt Client Id, Trailing Slahes and Timestamp Format

--- plantgw/__init__.py
+++ plantgw/__init__.py
@@ -43,6 +43,9 @@ class Configuration(object):
         self.mqtt_user = None
         self.mqtt_password = None
         self.mqtt_ca_cert = None
+        self.mqtt_client_id = None
+        self.mqtt_trailing_slash = True
+        self.mqtt_timestamp_format = None
         self.sensors = []
 
         if 'port' in config['mqtt']:
@@ -56,6 +59,15 @@ class Configuration(object):
 
         if 'ca_cert' in config['mqtt']:
             self.mqtt_ca_cert = config['mqtt']['ca_cert']
+
+        if 'client_id' in config['mqtt']:
+            self.mqtt_client_id = config['mqtt']['client_id']
+
+        if 'trailing_slash' in config['mqtt'] and not config['mqtt']['trailing_slash']:
+            self.mqtt_trailing_slash = False
+
+        if 'timestamp_format' in config['mqtt']:
+            self.mqtt_timestamp_format = config['mqtt']['timestamp_format']
 
         self.mqtt_server = config['mqtt']['server']
         self.mqtt_prefix = config['mqtt']['prefix']
@@ -113,7 +125,7 @@ class PlantGateway(object):
         logging.info('Disconnected MQTT connection')
 
     def _start_client(self):
-        self.mqtt_client = mqtt.Client()
+        self.mqtt_client = mqtt.Client(self.config.mqtt_client_id)
         if self.config.mqtt_user is not None:
             self.mqtt_client.username_pw_set(self.config.mqtt_user, self.config.mqtt_password)
         if self.config.mqtt_ca_cert is not None:
@@ -130,7 +142,12 @@ class PlantGateway(object):
     def _publish(self, sensor_config, sensor_data):
         if not self.connected:
             raise Exception('not connected to MQTT server')
-        prefix = '{}/{}/'.format(self.config.mqtt_prefix, sensor_config.get_topic())
+
+        prefix_fmt = '{}/{}'
+        if self.config.mqtt_trailing_slash:
+            prefix_fmt += '/'
+        prefix = prefix_fmt.format(self.config.mqtt_prefix, sensor_config.get_topic())
+
         data = {
             'battery': sensor_data.battery,
             'temperature': '{0:.1f}'.format(sensor_data.temperature),
@@ -139,6 +156,8 @@ class PlantGateway(object):
             'conductivity': sensor_data.conductivity,
             'timestamp': datetime.now().isoformat(),
         }
+        if self.config.mqtt_timestamp_format is not None:
+            data['timestamp'] = datetime.now().strftime(self.config.mqtt_timestamp_format)
         json_payload = json.dumps(data)
         self.mqtt_client.publish(prefix, json_payload, qos=1, retain=True)
         logging.info('sent data to topic %s', prefix)
@@ -172,7 +191,7 @@ class PlantGateway(object):
                 try:
                     self.process_mac(sensor)
                 # pylint: disable=bare-except
-                 except Exception as e:
+                except Exception as e:
                     next_list.append(sensor) # if it failed, we'll try again in the next round
                     msg = "could not read data from {} ({}) with reason: {}".format(sensor.mac, sensor.alias, str(e))
                     if sensor.fail_silent:
