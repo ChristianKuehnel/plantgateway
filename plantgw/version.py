ae1832eb3a45f7125b311e9902c81017a49d3664
Christian Kühnel <christian.kuehnel@gmail.com>
2018-04-26 22:21:32 +0200

    fixed version numbers for setup.py

--- plantgw/version.py
+++
@@ -1,3 +0,0 @@
-"""Plantgateway version number."""
-
-__version__ = '0.5.0'
--- setup.py
+++ setup.py
@@ -8,23 +8,19 @@
 ##############################################
 """Setup for plantgateway."""
 
-import sys
 from setuptools import setup
-from plantgw.version import __version__ as version
+from plantgw import __version__
 
 INSTALL_REQUIRES = ['bluepy==1.1.4', "paho-mqtt", 'pyyaml', "miflora==0.4"]
-if sys.version_info < (3, 0):
-    INSTALL_REQUIRES.append('mock')
-
 
 setup(
     name='plantgateway',
-    version=version,
+    version=__version__,
     description='Bluetooth to mqtt gateway for Xiaomi Mi plant sensors',
     author='Christian Kühnel',
     author_email='christian.kuehnel@gmail.com',
     url='https://www.python.org/sigs/distutils-sig/',
     packages=['plantgw'],
     install_requires=INSTALL_REQUIRES,
-    scripts=['plantgw/plantgateway'],
+    scripts=['plantgateway'],
     )
