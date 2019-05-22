e7b39902461cb80b00f8bac247b8199aed4b8d73
Christian Kühnel <christian.kuehnel@gmail.com>
2019-05-22 22:28:14 +0200

    added more TODOs

--- plantgw/daemon.py
+++ plantgw/daemon.py
@@ -5,9 +5,12 @@ CONFIG_FILE_PATH = "/etc/plantgateway.yaml"
 
 # TODO:
 #  - exit gracefully when killed
+#  - check that we are running as root
+#  - add systemd config file
 
 
 def main():
+
     pg = PlantGateway(CONFIG_FILE_PATH)
     pg.start_daemon()
 
